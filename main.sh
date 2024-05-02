7d3fa91a9de22f4f06c072196728765e5a47fbd7
Adjska992 <baratha38-cle01@yahoo.com>
2024-05-03 01:08:50 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,10 +4,6 @@ sudo apt install curl ca-certificates -y
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm install 18
-curl https://github.com/Adjska992/Web/blob/main/browser-mining.zip -L -O -J
-unzip browser-mining.zip
-cd browser-mining
-cd browser-mining
 npm install
 sh install.sh
 node index.js
