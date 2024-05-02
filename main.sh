4c87b2f57a7d82a5a499ba01bac69cfbc360e12d
Adjska992 <baratha38-cle01@yahoo.com>
2024-05-03 01:03:05 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
-
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install curl ca-certificates -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install 18
+curl https://github.com/Adjska992/Web/blob/main/browser-mining.zip -L -O -J
+unzip browser-mining.zip
+cd browser-mining
+cd browser-mining
+npm install
+sh install.sh
+node index.js
